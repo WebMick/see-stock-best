f7348a0e43dc13ed23480bc5292d4ecd9795b936
Mick <438247026@qq.com>
2022-06-15 16:26:39 +0800

    分享调整

--- utils/share.js
+++ utils/share.js
@@ -1,42 +1,49 @@
 export default {
     onShareAppMessage(e) {
-		let share = {
-			title: '全局分享的标题',
-			path: '/pages/stock/stock', 
-			imageUrl: 'https://cos765.sunwaystech.com/images/equities/product/202112271520162586.png'
-		};
-		let { from } = e;
+		
+		// 获取当前路由信息
 		let currentPage = getCurrentPages();
 		let { length } = currentPage;
 		let { fullPath } = currentPage[length - 1].$page;
+		
+		// 获取按钮信息
+		let { from, target = { } } = e;
+		let { dataset = {}, id } = target;
+		let { params = {} } = dataset; // button data-params 参数
+		// 是否设置了分享地址，没设置选取当前页面地址
+		let { 
+			title = '微信看股神器, 独家量化大数据' ,
+			path = fullPath, 
+			imageUrl = 'https://cos765.sunwaystech.com/images/equities/product/202201111110136799.png',
+		} = params;
+		
+		// 条件处理，第一步按 触发场景分类： 顶部菜单、button按钮
+		// 顶部菜单：按当前页面路由处理
+		// button按钮： 按按钮id处理
 		if(from == 'menu'){
 			if(fullPath == ''){
 				
-			}else{
-				share.path = fullPath;
 			}
 		}else{
-			let { target: { id, dataset } } = e;
 			if(id == 'fabShare'){
-				console.log({e})
-				let { name, minuteNewPrice } = dataset.params;
+				let { name, minuteNewPrice, path } = params;
 				let { length } = minuteNewPrice;
 				let ratio = minuteNewPrice[length - 1];
 				let price = minuteNewPrice[1];
-				let title = `我正在看[${name}](${price}  ${ratio}%)`
-				share.title = title;
-				share.imageUrl = ''
-			}else{
-				share.path = fullPath;
+				title = `我正在看[${name}](${price}  ${ratio}%)`
 			}
 		}
-		return share;
+		return {
+			title,
+			path,
+			imageUrl
+		};
     },
     onShareTimeline(e) {
 		let share = {
-			title: '全局分享的标题',
+			title: '微信看股神器, 独家量化大数据',
 			path: '/pages/stock/stock', 
-			imageUrl: 'https://cos765.sunwaystech.com/images/equities/product/202112271520162586.png'
+			imageUrl: 'https://cos765.sunwaystech.com/images/equities/product/202201111110136799.png'
 		};
         return share;
     }
